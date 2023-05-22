612b59d54f95811172ec088604eef5da07d2ce75
Adam Awany <45209813+Triple1996@users.noreply.github.com>
2023-05-22 12:34:27 -0400

    Add operate() function

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 const add = (a, b) => a+b;
 const subtract = (a, b) => a-b;
-const multiply = (a, b) => a*b;
+const mult = (a, b) => a*b;
 const divide = (a, b) => a/b;
+
+let firstNum;
+let operator;
+let secondNum;
+
+
+function operate (firstNum, secondNum, operator){
+
+    switch (operator){
+        case "+":
+            return add(firstNum, secondNum);
+        case "-": 
+            return subtract(firstNum, secondNum);
+        case "*":
+            return mult(firstNum, secondNum);
+        case "/":
+            return divide(firstNum, secondNum);
+        default:
+            console.error("No operator selected");
+            break;
+    }
+
+}
