b2c4fb451337f6d77d70b4d202a499029c9e9aec
Adam <Adamawany96@gmail.com>
2023-05-26 09:27:05 -0400

    Add js elements for each HTML button required

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const oneBtn = document.querySelector('#one');
+const twoBtn = document.querySelector('#two');
+const threeBtn = document.querySelector('#three');
+const fourBtn = document.querySelector('#four');
+const fiveBtn = document.querySelector('#five');
+const sixBtn = document.querySelector('#six');
+const sevenBtn = document.querySelector('#seven');
+const eightBtn = document.querySelector('#eight');
+const nineBtn = document.querySelector('#nine');
+const zeroBtn = document.querySelector('#zerp');
+
+const addBtn = document.querySelector('#add');
+const subtractBtn = document.querySelector('#subtract');
+const divideBtn = document.querySelector('#divide');
+const multiplyBtn = document.querySelector('#mutiply');
+const equalsBtn = document.querySelector('#equals');
+const clearBtn = document.querySelector('#clear');
+
+const display = document.querySelector('#display');
+
+
+
 const add = (a, b) => a+b;
 const subtract = (a, b) => a-b;
 const mult = (a, b) => a*b;
